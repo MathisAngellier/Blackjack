fe28ac97f5bc407d1c3189be91472a3e346acbb8
AlekDurnez <alek.durnez@student.kdg.be>
2022-12-16 16:11:05 +0100

    Spelers opslaan
    de spelers werden niet opgeslagen, als je speler 1 printte kreeg je gwn de meest recente speler, nu is er een array van spelers waarin we de spelers opslaan

--- Blackjack/src/DemoBlackjack.java
+++ Blackjack/src/DemoBlackjack.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DemoBlackjack {
     public static void main(String[] args) {
 
+        Speler[] spelers = new Speler[5];
         Scanner input = new Scanner(System.in);
         System.out.println("Welkom bij het spel BLACKJACK");
         int aantalSpelers = 0;
@@ -14,22 +17,13 @@ public class DemoBlackjack {
         String naam = "";
         double geld = 0;
 
-
         for (int i = 0; i < aantalSpelers; i++) {
             System.out.print("Wat is de naam van speler" +(i+1)+"? ");
             naam = input.next();
             System.out.print("Hoeveel geld heeft de speler" +(i+1)+ "? ");
             geld = input.nextDouble();
-
+            spelers[i] = new Speler(naam,geld);
         }
-
-        Speler speler1 = new Speler(naam,geld);
-
-        System.out.println(speler1);
-
-
-
-
-
+        System.out.print("array van spelers" + Arrays.toString(spelers));
     }
 }
