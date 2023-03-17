66d3980760d15b02c0cd68d889e55db813603e1e
AlekDurnez <alek.durnez@student.kdg.be>
2023-03-17 17:39:09 +0100

    Update InzetPaginaView.java

--- Blackjack/src/view/inzetpagina/InzetPaginaView.java
+++ Blackjack/src/view/inzetpagina/InzetPaginaView.java
@@ -1,33 +1,33 @@
 package view.inzetpagina;
 
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import model.Spel;
+import model.Speler;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class InzetPaginaView extends BorderPane {
+
+
     private GridPane gp;
+    private Spel model;
 
     private Button bevestigen;
 
 
-
     public InzetPaginaView() {
         this.gp = new GridPane();
+        this.model = new Spel();
         initialiseNodes();
         layoutNodes();
-    }
-    public void addToGrid(String naam, int geld, int rowIndex) {
-        Label nameLabel = new Label(naam);
-        Label geldLabel = new Label(Integer.toString(geld));
-        gp.add(nameLabel, 0, rowIndex);
-        gp.add(geldLabel, 1, rowIndex);
+        displayLabels();
     }
 
     private void initialiseNodes() {
@@ -35,11 +35,23 @@ public class InzetPaginaView extends BorderPane {
     }
 
     private void layoutNodes() {
-        setBackground(new Background(new BackgroundImage(new Image("resources/startpaginaAfbeeldingKlein.png"),null,null,null,null)));
+        setBackground(new Background(new BackgroundImage(new Image("resources/startpaginaAfbeeldingKlein.png"), null, null, null, null)));
+        gp.add(bevestigen, 0, 0);
         setTop(gp);
 
     }
 
+    public void displayLabels() {
+        // Iterate over the array of Speler objects in the model and create a label for each naam and geld value
+        int row = 0;
+        for (Speler speler : model.getSpelersArray()) {
+            Label naamLabel = new Label(speler.getNaam());
+            Label geldLabel = new Label(String.valueOf(speler.getGeld()));
+            gp.add(naamLabel, 0, row);
+            gp.add(geldLabel, 1, row);
+            row++;
+        }
+    }
     public Button getBevestigen() {
         return bevestigen;
     }
